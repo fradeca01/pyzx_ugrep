9756c0cf9817c4a54171f286f846a169be78f73c
Francesco <fradeca01@gmail.com>
2025-08-04 10:05:06 +0200

    tests

--- tests/_test_graph_state.py
+++ tests/_test_graph_state.py
@@ -1,7 +1,6 @@
 import unittest
 import sys
-
-
+from pyzx.circuit import Circuit, cliffords
 if __name__ == '__main__':
     sys.path.append('..')
     sys.path.append('.')
@@ -9,4 +8,7 @@ if __name__ == '__main__':
 
 
 class TestGraphState(unittest.TestCase):
-    
+
+    def setUp(self):
+        circuit = cliffords(10, 20)
+        graph = circuit.to_graph()
